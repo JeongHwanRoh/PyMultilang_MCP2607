fffc1c3798f72af1f756a2186eac6e9fe0abb42f
노정환 <108844707+JeongHwanRoh@users.noreply.github.com>
2026-09-08 20:45:37 +0900

    Implement language detection feature
    Added language detection function to analyze input text.

--- main.py
+++ main.py
@@ -2,5 +2,33 @@ import streamlit as st
 import joblib
 import sklearn
 
-clf=joblib.load("freq.pkl")
-st.title('외국어 문장 판별')
+clf = joblib.load("freq.pkl")
+
+def detect_lang(text):
+    # 입력텍스트 소문자 변환
+    text = text.lower()
+
+    # 알파벳의 출현 빈도
+    cnt = []
+    for i in range(26):
+      ch = chr(i + ord('a'))
+      cnt.append(text.count(ch))
+
+    total = sum(cnt)  # 전체 알파벳 문자 개수.
+    if total == 0: return "입력이 없습니다."  
+
+    # 입력데이터 준비, 전처리
+    freq = list(map(lambda n: n / total, cnt))
+
+    # 예측하기
+    pred = clf.predict([freq])
+
+    # 예측결과 리턴
+    lang_dic = {
+        "en": "영어",
+        "fr": "프랑스어",
+        "id": "인도네시아어",
+        "tl": "타갈로그어",
+    }
+
+    return lang_dic[pred[0]]
